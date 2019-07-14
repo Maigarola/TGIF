2cd48f824fdacb3a5949682d2f12008ca4e66e62
Amaia Garola <51963743+Maigarola@users.noreply.github.com>
2019-07-14 21:55:55 +0200

    CONLINKNAN

--- scripts/main_house_atendance.js
+++ scripts/main_house_atendance.js
@@ -11,8 +11,12 @@ let statistics = {
   ind_votes: 0
 };
 
+let per = members.length / 10;
+let m_ordenvotes = members.sort(function (a, b) { return (a.missed_votes - b.missed_votes) })
+
 calcstats();
 printtable();
+mostengaged();
 
 function calcstats() {
   members.forEach(member => {
@@ -30,12 +34,30 @@ function calcstats() {
     }
   });
 
-  statistics.totalAvgR = statistics.rep_votes / statistics.num_rep;
-  statistics.totalAvgD = statistics.dem_votes / statistics.num_dem;
-  statistics.totalAvgI = statistics.ind_votes / statistics.num_ind;
+  if (statistics.num_rep == 0) {
+    statistics.totalAvgR = 0
+  }
+  else {
+    statistics.totalAvgR = statistics.rep_votes / statistics.num_rep;
+  }
+  if (statistics.num_dem == 0) {
+    statistics.totalAvgD = 0
+  }
+  else {
+    statistics.totalAvgD = statistics.dem_votes / statistics.num_dem;
+  }
+  if (statistics.num_ind == 0) {
+    statistics.totalAvgI = 0
+  }
+  else {
+    statistics.totalAvgI = statistics.ind_votes / statistics.num_ind
+  }
+
+
+
 }
 function printtable() {
-  
+
   document.getElementById("demtot").innerHTML = statistics.num_rep;
   document.getElementById("reptot").innerHTML = statistics.num_dem;
   document.getElementById("itot").innerHTML = statistics.num_ind;
@@ -54,4 +76,21 @@ function printtable() {
       statistics.totalAvgR.toFixed(2) +
       statistics.totalAvgI.toFixed(2)) /
     3;
+}
+function mostengaged() {
+
+  let tablebody = document.getElementById("tbody_most");
+  let template = " ";
+
+  for (let i = 0; i < per; i++) {
+
+    template += `
+      <tr>
+        <td> <a href="${m_ordenvotes[i].url}">${m_ordenvotes[i].first_name} ${m_ordenvotes[i].second_name || ""} ${m_ordenvotes[i].last_name} </a></td>
+        <td> ${m_ordenvotes[i].missed_votes}</td>
+        </tr >
+    `;
+  }
+
+  tablebody.innerHTML = template;
 }
