b0413ba642be589c8dfe9884de7b5958fa0687d7
Amaia Garola <51963743+Maigarola@users.noreply.github.com>
2019-07-14 10:05:52 +0200

    NAVBARREADMOREBOOTSTRAP

--- scripts/main_house_atendance.js
+++ scripts/main_house_atendance.js
@@ -2,21 +2,56 @@
 
 let members = data.results[0].members;
 
-function from_party(lista, p) {
+let statistics = {
+  num_rep: 0,
+  rep_votes: 0,
+  num_dem: 0,
+  dem_votes: 0,
+  num_ind: 0,
+  ind_votes: 0
+};
 
-  let j = 0;
+calcstats();
+printtable();
 
-
-  for (let i = 0; i < lista.length; i++) {
-
-    if (lista[i].party == p) {
-      j++;
+function calcstats() {
+  members.forEach(member => {
+    if (member.party == "R") {
+      statistics.num_rep++;
+      statistics.rep_votes += member.votes_with_party_pct;
     }
-  }
-}
-function house_glance(lista) {
-  from_party(members, "D");
+    if (member.party == "D") {
+      statistics.num_dem++;
+      statistics.dem_votes += member.votes_with_party_pct;
+    }
+    if (member.party == "I") {
+      statistics.num_ind++;
+      statistics.ind_votes += member.votes_with_party_pct;
+    }
+  });
 
+  statistics.totalAvgR = statistics.rep_votes / statistics.num_rep;
+  statistics.totalAvgD = statistics.dem_votes / statistics.num_dem;
+  statistics.totalAvgI = statistics.ind_votes / statistics.num_ind;
 }
-from_party(members, "D");
+function printtable() {
+  
+  document.getElementById("demtot").innerHTML = statistics.num_rep;
+  document.getElementById("reptot").innerHTML = statistics.num_dem;
+  document.getElementById("itot").innerHTML = statistics.num_ind;
+  document.getElementById("total").innerHTML =
+    statistics.num_rep + statistics.num_dem + statistics.num_ind;
 
+  document.getElementById("demvotes").innerHTML = statistics.totalAvgD.toFixed(
+    2
+  );
+  document.getElementById("repvotes").innerHTML = statistics.totalAvgR.toFixed(
+    2
+  );
+  document.getElementById("ivotes").innerHTML = statistics.totalAvgI.toFixed(2);
+  document.getElementById("totalvotes").innerHTML =
+    (statistics.totalAvgD.toFixed(2) +
+      statistics.totalAvgR.toFixed(2) +
+      statistics.totalAvgI.toFixed(2)) /
+    3;
+}
