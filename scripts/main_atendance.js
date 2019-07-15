fcdd75222940c973e9888cf79b9b4b4b0871d8f1
Amaia Garola <51963743+Maigarola@users.noreply.github.com>
2019-07-15 18:09:03 +0200

    ATTENDANCECOMPLETO

--- scripts/main_atendance.js
+++ scripts/main_atendance.js
@@ -11,12 +11,16 @@ let statistics = {
   ind_votes: 0
 };
 
-let per = members.length / 10;
-let m_ordenvotes = members.sort(function (a, b) { return (a.missed_votes - b.missed_votes) })
+let per = Math.round(members.length / 10); //We'll use it as a index
+
+let m_ordenvotes = members;
 
 calcstats();
 printtable();
-mostengaged();
+m_ordenvotes.sort(function (a, b) { return (b.missed_votes_pct - a.missed_votes_pct) });
+leastmostengaged(m_ordenvotes, "tbody_least");
+m_ordenvotes.sort(function (a, b) { return (a.missed_votes_pct - b.missed_votes_pct) });
+leastmostengaged(m_ordenvotes, "tbody_most");
 
 function calcstats() {
   members.forEach(member => {
@@ -54,7 +58,6 @@ function calcstats() {
   }
 
 
-
 }
 function printtable() {
 
@@ -77,20 +80,36 @@ function printtable() {
       statistics.totalAvgI.toFixed(2)) /
     3;
 }
-function mostengaged() {
+function leastmostengaged(m_ordenvotes, idhtml) {
 
-  let tablebody = document.getElementById("tbody_most");
+  let tablebody = document.getElementById(idhtml);
   let template = " ";
-
+  let perdup = per; 
   for (let i = 0; i < per; i++) {
 
     template += `
       <tr>
-        <td> <a href="${m_ordenvotes[i].url}">${m_ordenvotes[i].first_name} ${m_ordenvotes[i].second_name || ""} ${m_ordenvotes[i].last_name} </a></td>
-        <td> ${m_ordenvotes[i].missed_votes}</td>
+      <td> <a href="${m_ordenvotes[i].url}">${m_ordenvotes[i].first_name} ${(m_ordenvotes[i].second_name || "")} ${m_ordenvotes[i].last_name} </a></td>
+      <td> ${m_ordenvotes[i].missed_votes}</td>
+        <td> ${m_ordenvotes[i].missed_votes_pct}</td>
+
         </tr >
     `;
+
   }
+
+  while (m_ordenvotes[perdup - 1].missed_votes_pct == m_ordenvotes[perdup].missed_votes_pct) {
+    template += `
+    <tr>
+      <td> <a href="${m_ordenvotes[perdup].url}">${m_ordenvotes[perdup].first_name} ${(m_ordenvotes[perdup].second_name || "")} ${m_ordenvotes[perdup].last_name} </a></td>
+      <td> ${m_ordenvotes[perdup].missed_votes}</td>
+      <td> ${m_ordenvotes[perdup].missed_votes_pct}</td>
+
+      </tr >
+  `;
+    perdup++;
+  }
+
 
   tablebody.innerHTML = template;
 }
