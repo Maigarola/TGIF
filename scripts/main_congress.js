8ef079390ac56d7ed98926325789539e2beb13e0
Amaia Garola <51963743+Maigarola@users.noreply.github.com>
2019-07-17 13:34:33 +0200

    CHECBOXDELTODOYDROP

--- scripts/main_congress.js
+++ scripts/main_congress.js
@@ -9,19 +9,23 @@ let table_body = document.getElementById("table-body");
 // tabletable(members);
 
 //FILTROS
-filter(members,"D");
-filter(members,"R");
-filter(members,"I");
+filter(members, "D");
+filter(members, "R");
+filter(members, "I");
+
+dropdownmenu(members);
 
 document.getElementById("R_checked").addEventListener("click", () => filter(members, "R"));
 document.getElementById("D_checked").addEventListener("click", () => filter(members, "D"));
 document.getElementById("I_checked").addEventListener("click", () => filter(members, "I"));
+document.getElementById("State").addEventListener("change", () => filter(members, "All"));
 
 function filter(lista, a) {
 
     D_checked = document.getElementById("D_checked").checked;
     R_checked = document.getElementById("R_checked").checked;
     I_checked = document.getElementById("I_checked").checked;
+    S_selected = document.getElementById("State").onselect;
 
     let whochecked = [];
 
@@ -35,6 +39,11 @@ function filter(lista, a) {
     if (I_checked) {
         whochecked.push("I");
     }
+    if (S_selected != ""){
+        whochecked.push(S_selected);
+    }
+
+    console.log(whochecked);
 
     m_filtrada = [];
 
@@ -43,15 +52,21 @@ function filter(lista, a) {
             m_filtrada.push(lista[i]);
         }
     }
-    tabletable(m_filtrada);
+    if (m_filtrada.length == 0) {
 
+        table_body.innerHTML = "NO CRITERIA WITH THIS FILTERS"
+    }
 
-    function tabletable(lista) {
+    else { tabletable(m_filtrada) }
 
-        let template = " ";
+}
 
-        for (let i = 0; i < lista.length; i++) {
-            template += `
+function tabletable(lista) {
+
+    let template = " ";
+
+    for (let i = 0; i < lista.length; i++) {
+        template += `
       <tr>
       <td> <a href="${lista[i].url}">${lista[i].first_name} ${(lista[i].middle_name || "")} ${lista[i].last_name} </a></td>
       <td> ${lista[i].party}</td>
@@ -60,7 +75,31 @@ function filter(lista, a) {
       <td> ${lista[i].votes_with_party_pct}</td>
       </tr>
        `;
-            table_body.innerHTML = template;
-        }
+        table_body.innerHTML = template;
     }
 }
+
+function dropdownmenu(lista) {
+
+    let norepes = [];
+
+    let newoption = document.getElementById("State");
+
+    for (let i = 0; i < lista.length; i++) {
+
+        if (!norepes.includes(lista[i].state)) { norepes.push(lista[i].state); }
+    }
+
+    norepes.sort();
+
+    let template = `<option value = ${"-- All States --"}></option>"`;
+
+    for (let i = 0; i < norepes.length; i++) {
+        template += `
+       <option value = ${norepes[i]}></option>
+       `;
+        newoption.innerHTML = template;
+    }
+}
+
+
