fed18a46527406f54ee20c6ca362e75a7fec4335
Amaia Garola <51963743+Maigarola@users.noreply.github.com>
2019-07-23 19:43:28 +0200

    FETCHED

--- scripts/main_congress.js
+++ scripts/main_congress.js
@@ -1,64 +1,78 @@
-//table PAGE
+// table PAGE
 
 // Create the general table with general information
+let url = " ";
 
-let members = data.results[0].members;
-
-let table_body = document.getElementById("table-body");
-
-//FILTROS
-
-dropdownmenu(members);
-filter(members);
-
-
-document.getElementById("R_checked").addEventListener("click", () => filter(members));
-document.getElementById("D_checked").addEventListener("click", () => filter(members));
-document.getElementById("I_checked").addEventListener("click", () => filter(members));
-document.getElementById("State").addEventListener("change", () => filter(members));
-
-
-function filter(lista) {
-
-    let thestate = document.getElementById("State").value;
-    let D_checked = document.getElementById("D_checked").checked;
-    let R_checked = document.getElementById("R_checked").checked;
-    let I_checked = document.getElementById("I_checked").checked;
-
-    let m_filtrada = [];
-    let added = false;
-
-    for (let i = 0; i < lista.length; i++) {
-
-        if ((added == false) && (D_checked) && ((thestate == "AllStates") || (lista[i].state == thestate)) && (lista[i].party == "D")) {
-            m_filtrada.push(lista[i]);
-            added = true;
-        }
-        if ((added == false) && (R_checked) && ((thestate == "AllStates") || (lista[i].state == thestate)) && (lista[i].party == "R")) {
-            m_filtrada.push(lista[i]);
-            added = true;
-        }
-        if ((added == false) && (I_checked) && ((thestate == "AllStates") || (lista[i].state == thestate)) && (lista[i].party == "I")) {
-            m_filtrada.push(lista[i]);
-            added = true;
-        }
-        added = false;
-    }
-    if (m_filtrada.length == 0) {
-
-        table_body.innerHTML = "NO CRITERIA WITH THIS FILTERS"
-    }
-
-    else { tabletable(m_filtrada) }
+if (window.location.href.includes("senate")) {
+    url = "https://api.propublica.org/congress/v1/113/senate/members.json";
+}
+else if (window.location.href.includes("house")) {
+    url = "https://api.propublica.org/congress/v1/113/house/members.json";
 }
 
+fetch(url, {
+    method: "GET",
+    headers: {
+        "X-API-Key": "3Ibg4YcgFB2moMyp06P8mt1DXxuN0dWlDmvayaWb"
+    }
+}).then(function (response) {
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error(response.statusText);
 
-function tabletable(lista) {
+}).then(function (json) {
 
-    let template = " ";
+    data = json;
 
-    for (let i = 0; i < lista.length; i++) {
-        template += `
+    let members = data.results[0].members;
+
+    let table_body = document.getElementById("table-body");
+
+    //FILTROS
+
+    dropdownmenu(members);
+    filter(members);
+
+    document.getElementById("R_checked").addEventListener("click", () => filter(members));
+    document.getElementById("D_checked").addEventListener("click", () => filter(members));
+    document.getElementById("I_checked").addEventListener("click", () => filter(members));
+    document.getElementById("State").addEventListener("change", () => filter(members));
+
+    function filter(lista) {
+
+        let thestate = document.getElementById("State").value;
+        let D_checked = document.getElementById("D_checked").checked;
+        let R_checked = document.getElementById("R_checked").checked;
+        let I_checked = document.getElementById("I_checked").checked;
+
+        let m_filtrada = [];
+
+        for (let i = 0; i < lista.length; i++) {
+
+            if ((D_checked) && ((thestate == "AllStates") || (lista[i].state == thestate)) && (lista[i].party == "D")) {
+                m_filtrada.push(lista[i]);
+            }
+            if ((R_checked) && ((thestate == "AllStates") || (lista[i].state == thestate)) && (lista[i].party == "R")) {
+                m_filtrada.push(lista[i]);
+            }
+            if ((I_checked) && ((thestate == "AllStates") || (lista[i].state == thestate)) && (lista[i].party == "I")) {
+                m_filtrada.push(lista[i]);
+            }
+        }
+        if (m_filtrada.length == 0) {
+
+            table_body.innerHTML = "NO CRITERIA WITH THIS FILTERS"
+        }
+
+        else { tabletable(m_filtrada) }
+    }
+    function tabletable(lista) {
+
+        let template = " ";
+
+        for (let i = 0; i < lista.length; i++) {
+            template += `
       <tr>
       <td> <a href="${lista[i].url}">${lista[i].first_name} ${(lista[i].middle_name || "")} ${lista[i].last_name} </a></td>
       <td> ${lista[i].party}</td>
@@ -67,31 +81,32 @@ function tabletable(lista) {
       <td> ${lista[i].votes_with_party_pct}</td>
       </tr>
        `;
-        table_body.innerHTML = template;
+            table_body.innerHTML = template;
+        }
     }
-}
+    function dropdownmenu(lista) {
 
-function dropdownmenu(lista) {
+        let norepes = [];
 
-    let norepes = [];
+        let newoption = document.getElementById("State");
 
-    let newoption = document.getElementById("State");
+        for (let i = 0; i < lista.length; i++) {
 
-    for (let i = 0; i < lista.length; i++) {
+            if (!norepes.includes(lista[i].state)) { norepes.push(lista[i].state); }
+        }
 
-        if (!norepes.includes(lista[i].state)) { norepes.push(lista[i].state); }
-    }
+        norepes.sort();
 
-    norepes.sort();
-
-    let template = `<option value = "AllStates">-- All States --</option>`
-        ;
-    for (let i = 0; i < norepes.length; i++) {
-        template += `
+        let template = `<option value = "AllStates">-- All States --</option>`
+            ;
+        for (let i = 0; i < norepes.length; i++) {
+            template += `
         <option value = "${norepes[i]}">${norepes[i]}</option>
         `;
+        }
+        newoption.innerHTML = template;
     }
-    newoption.innerHTML = template;
 
-}
-
+}).catch(function (error) {
+    console.log("Request failed: " + error.message);
+});
--- scripts/main_loyalty.js
+++ scripts/main_loyalty.js
@@ -1,95 +1,116 @@
 // Nº of party
+let url = " ";
 
-let members = data.results[0].members;
-
-let statistics = {
-    num_rep: 0,
-    rep_votes: 0,
-    num_dem: 0,
-    dem_votes: 0,
-    num_ind: 0,
-    ind_votes: 0
-};
-
-let per = Math.round(members.length / 10); //We'll use it as a index
-
-let m_ordenvotes = members;
-let ceros = 0;
-calcstats();
-
-printtable();
-
-m_ordenvotes.sort(function (a, b) { return (b.votes_with_party_pct - a.votes_with_party_pct) });
-leastmostloyal(m_ordenvotes, "tbody_most_party");
-m_ordenvotes.sort(function (a, b) { return (a.votes_with_party_pct - b.votes_with_party_pct) });
-leastmostloyal(m_ordenvotes, "tbody_least_party");
-
-function calcstats() {
-    members.forEach(member => {
-        if (member.party == "R") {
-            statistics.num_rep++;
-            statistics.rep_votes += member.votes_with_party_pct;
-        }
-        if (member.party == "D") {
-            statistics.num_dem++;
-            statistics.dem_votes += member.votes_with_party_pct;
-        }
-        if (member.party == "I") {
-            statistics.num_ind++;
-            statistics.ind_votes += member.votes_with_party_pct;
-        }
-    });
-
-    if (statistics.num_rep == 0) {
-        statistics.totalAvgR = 0
-        ceros++;
-    } else {
-        statistics.totalAvgR = statistics.rep_votes / statistics.num_rep;
-    }
-    if (statistics.num_dem == 0) {
-        statistics.totalAvgD = 0
-        ceros++;
-    } else {
-        statistics.totalAvgD = statistics.dem_votes / statistics.num_dem;
-    }
-    if (statistics.num_ind == 0) {
-        statistics.totalAvgI = 0
-        ceros++;
-    } else {
-        statistics.totalAvgI = statistics.ind_votes / statistics.num_ind
-    }
-
-
+if (window.location.href.includes("senate")) {
+    url = "https://api.propublica.org/congress/v1/113/senate/members.json";
+}
+else if (window.location.href.includes("house")) {
+    url = "https://api.propublica.org/congress/v1/113/house/members.json";
 }
 
-function printtable() {
+fetch(url, {
+    method: "GET",
+    headers: {
+        "X-API-Key": "3Ibg4YcgFB2moMyp06P8mt1DXxuN0dWlDmvayaWb"
+    }
+}).then(function (response) {
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error(response.statusText);
 
-    document.getElementById("demtot").innerHTML = statistics.num_rep;
-    document.getElementById("reptot").innerHTML = statistics.num_dem;
-    document.getElementById("itot").innerHTML = statistics.num_ind;
-    document.getElementById("total").innerHTML =
-        statistics.num_rep + statistics.num_dem + statistics.num_ind;
+}).then(function (json) {
 
-    document.getElementById("demvotes").innerHTML = statistics.totalAvgD.toFixed(
-        2
-    );
-    document.getElementById("repvotes").innerHTML = statistics.totalAvgR.toFixed(
-        2
-    );
-    document.getElementById("ivotes").innerHTML = statistics.totalAvgI.toFixed(2);
-    document.getElementById("totalvotes").innerHTML =
-        ((statistics.totalAvgD + statistics.totalAvgR + statistics.totalAvgI) /
-            (3 - ceros)).toFixed(2);
-}
+    data = json;
 
-function leastmostloyal(m_ordenvotes, idhtml) {
+    let members = data.results[0].members;
 
-    let tablebody = document.getElementById(idhtml);
-    let template = " ";
-    let perdup = per;
-    for (let i = 0; i < per; i++) {
+    let statistics = {
+        num_rep: 0,
+        rep_votes: 0,
+        num_dem: 0,
+        dem_votes: 0,
+        num_ind: 0,
+        ind_votes: 0
+    };
 
-        template += `
+    let per = Math.round(members.length / 10); //We'll use it as a index
+
+    let m_ordenvotes = members;
+    let ceros = 0;
+
+    calcstats();
+    printtable();
+
+    m_ordenvotes.sort(function (a, b) { return (b.votes_with_party_pct - a.votes_with_party_pct) });
+    leastmostloyal(m_ordenvotes, "tbody_most_party");
+    m_ordenvotes.sort(function (a, b) { return (a.votes_with_party_pct - b.votes_with_party_pct) });
+    leastmostloyal(m_ordenvotes, "tbody_least_party");
+
+    function calcstats() {
+        members.forEach(member => {
+            if (member.party == "R") {
+                statistics.num_rep++;
+                statistics.rep_votes += member.votes_with_party_pct;
+            }
+            if (member.party == "D") {
+                statistics.num_dem++;
+                statistics.dem_votes += member.votes_with_party_pct;
+            }
+            if (member.party == "I") {
+                statistics.num_ind++;
+                statistics.ind_votes += member.votes_with_party_pct;
+            }
+        });
+
+        if (statistics.num_rep == 0) {
+            statistics.totalAvgR = 0
+            ceros++;
+        } else {
+            statistics.totalAvgR = statistics.rep_votes / statistics.num_rep;
+        }
+        if (statistics.num_dem == 0) {
+            statistics.totalAvgD = 0
+            ceros++;
+        } else {
+            statistics.totalAvgD = statistics.dem_votes / statistics.num_dem;
+        }
+        if (statistics.num_ind == 0) {
+            statistics.totalAvgI = 0
+            ceros++;
+        } else {
+            statistics.totalAvgI = statistics.ind_votes / statistics.num_ind
+        }
+    }
+
+    function printtable() {
+
+        document.getElementById("demtot").innerHTML = statistics.num_rep;
+        document.getElementById("reptot").innerHTML = statistics.num_dem;
+        document.getElementById("itot").innerHTML = statistics.num_ind;
+        document.getElementById("total").innerHTML =
+            statistics.num_rep + statistics.num_dem + statistics.num_ind;
+
+        document.getElementById("demvotes").innerHTML = statistics.totalAvgD.toFixed(
+            2
+        );
+        document.getElementById("repvotes").innerHTML = statistics.totalAvgR.toFixed(
+            2
+        );
+        document.getElementById("ivotes").innerHTML = statistics.totalAvgI.toFixed(2);
+        document.getElementById("totalvotes").innerHTML =
+            ((statistics.totalAvgD + statistics.totalAvgR + statistics.totalAvgI) /
+                (3 - ceros)).toFixed(2);
+    }
+
+    function leastmostloyal(m_ordenvotes, idhtml) {
+
+        let tablebody = document.getElementById(idhtml);
+        let template = " ";
+        let perdup = per;
+        for (let i = 0; i < per; i++) {
+
+            template += `
       <tr>
       <td> <a href="${m_ordenvotes[i].url}">${m_ordenvotes[i].first_name} ${(m_ordenvotes[i].second_name || "")} ${m_ordenvotes[i].last_name} </a></td>
       <td> ${m_ordenvotes[i].total_votes}</td>
@@ -98,17 +119,20 @@ function leastmostloyal(m_ordenvotes, idhtml) {
         </tr >
     `;
 
-    }
-    while (m_ordenvotes[perdup - 1].votes_with_party_pct == m_ordenvotes[perdup].votes_with_party_pct) {
-        template += `
+        }
+        while (m_ordenvotes[perdup - 1].votes_with_party_pct == m_ordenvotes[perdup].votes_with_party_pct) {
+            template += `
     <tr>
       <td> <a href="${m_ordenvotes[perdup].url}">${m_ordenvotes[perdup].first_name} ${(m_ordenvotes[perdup].second_name || "")} ${m_ordenvotes[perdup].last_name} </a></td>
       <td> ${m_ordenvotes[i].total_votes}</td>
       <td> ${m_ordenvotes[i].votes_with_party_pct}</td>
       </tr >
   `;
-        perdup++;
-    }
-    tablebody.innerHTML = template;
+            perdup++;
+        }
+        tablebody.innerHTML = template;
 
-}
+    }
+}).catch(function (error) {
+    console.log("Request failed: " + error.message);
+});
